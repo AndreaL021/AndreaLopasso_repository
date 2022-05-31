c98038461499b1689df70ac104157f1ad96b2023
Andrea 02156 <lopassoandrea1@gmail.com>
2022-06-01 01:44:10 +0200

    aggiornamento gioco

--- Gioco_in_js_Htlm_Css/funzioni.js
+++ Gioco_in_js_Htlm_Css/funzioni.js
@@ -14,15 +14,17 @@ export let attacchi=[
 export function showGame(atk, p1, nemico){
     let showNemico= document.querySelector("#enemy");
     showNemico.setAttribute("class", "enemy");
-    let elemento= document.createElement("div");
-    elemento.setAttribute("class", "row d-flex justify-content-center");
-    elemento.setAttribute("style", "width: 30vw;"); 
+    let elemento= document.createElement("ul");
     for (let i = 0; i < this.attacchi.length; i++) {
         elemento.innerHTML+=
         `
-            <button value="${i}" id="button${i}" class="btn b bg-white col-5 m-1">${attacchi[i]}</button>
+            <li><button value="${i}" id="button${i}" class="btn b bg-white m-1">${attacchi[i]}</button></li>
         `;
     }
+    elemento.innerHTML+=
+        `
+            <li><button id="heal" class="btn b text-white bg-green d-flex justify-content-center">Pozione</button></li>
+        `;
     atk.appendChild(elemento);
     vita.innerHTML=('Vita: '+p1.hp);
     pozioni.innerHTML=('Pozioni: '+p1.heals);
@@ -61,6 +63,21 @@ export function message() {
             Livello ${x}
         `;
     }
+    x++;
+    if (x>=2) {
+        modal.replaceChild(message, modal.firstChild);
+    }else{
+        modal.appendChild(message);
+    }
+}
+export function showDmg() {
+    let modal= document.getElementById("message");
+    let message= document.createElement("span");
+    message.setAttribute("class", "text-center text-white fs-message");
+        message.innerHTML=
+        `
+            Gioco terminato
+        `;
     x++;
     if (x>=2) {
         modal.replaceChild(message, modal.firstChild);
